ba6790db301aeef54fb51ef5365d24c08de8690b
SAJAL JAIN <120775498+sajaljain7925@users.noreply.github.com>
2025-12-15 23:35:53 +0530

    Time: 3 ms (9.42%) | Memory: 8.9 MB (37.59%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,27 @@
+// Stack
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+                if (st.empty())
+                    return false;
+                char cur = st.top();
+                st.pop();
+                if (s[i] == ')' && cur != '(')
+                    return false;
+                if (s[i] == '}' && cur != '{')
+                    return false;
+                if (s[i] == ']' && cur != '[')
+                    return false;
+            } else {
+                st.push(s[i]);
+            }
+        }
+        if (st.empty())
+            return true;
+        else
+            return false;
+    }
+};
